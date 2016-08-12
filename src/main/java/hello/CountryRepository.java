94a51063dc7fbe8dcd09d08c272336db0c365863
codygulley <swcta2012>
2016-08-11 20:05:12 -0600

    Init

---
+++ src/main/java/hello/CountryRepository.java
@@ -0,0 +1,66 @@
+package hello;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.spring.guides.gs_producing_web_service.Country;
+import io.spring.guides.gs_producing_web_service.Currency;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+public class CountryRepository {
+    private static final List<Country> countries = new ArrayList<Country>();
+
+    public Country findCountry(String name) {
+        Assert.notNull(name);
+
+        Country result = null;
+
+        for (Country country : countries) {
+            if (name.equals(country.getName())) {
+                result = country;
+            }
+        }
+
+        return result;
+    }
+
+    @PostConstruct
+    public void initData() {
+        Country spain = new Country();
+        spain.setName("Spain");
+        spain.setCapital("Madrid");
+        spain.setCurrency(Currency.EUR);
+        spain.setPopulation(46704314);
+
+        countries.add(spain);
+
+        Country poland = new Country();
+        poland.setName("Poland");
+        poland.setCapital("Warsaw");
+        poland.setCurrency(Currency.PLN);
+        poland.setPopulation(38186860);
+
+        countries.add(poland);
+
+        Country uk = new Country();
+        uk.setName("United Kingdom");
+        uk.setCapital("London");
+        uk.setCurrency(Currency.GBP);
+        uk.setPopulation(63705000);
+
+        countries.add(uk);
+
+        Country usa = new Country();
+        uk.setName("United States of America");
+        uk.setCapital("Washington D.C.");
+        uk.setCurrency(Currency.USD);
+        uk.setPopulation(324394946);
+
+        countries.add(usa);
+    }
+
+
+}
